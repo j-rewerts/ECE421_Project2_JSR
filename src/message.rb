173dbf0731633dbe5045d1ad89e4efe0cb89ba56
Jared Rewerts <rewerts@ualberta.ca>
2016-02-10 12:46:46 -0700

    Stubbed the basics of the message system.

---
+++ src/message.rb
@@ -0,0 +1,18 @@
+class Message
+
+	@time
+	@message
+
+	def initialize(time, message)
+		@time = time
+		@message = message
+	end
+
+	def execute()
+		# Call into the C program.
+
+
+		# Print the message
+	end
+
+end
---
+++ src/project2_test.rb
@@ -0,0 +1,12 @@
+require "test/unit"
+
+class Project2Test < Test::Unit::TestCase
+
+	def test_message_bad_input
+
+	end
+
+	def test_message_overflow
+
+	end
+end
