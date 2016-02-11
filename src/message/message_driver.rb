6dbb48a186eec0a1ddb8e622b548af8d704e393e
Jared Rewerts <rewerts@ualberta.ca>
2016-02-10 17:52:05 -0700

    Removed debug print messages.

--- src/message/message_driver.rb
+++ src/message/message_driver.rb
@@ -4,7 +4,7 @@ argString = ""
 ARGV.each do |value|
     argString = argString + " " + "\"" + value + "\""
 end
-puts argString
+
 file = File.expand_path File.dirname(__FILE__) + "/message.rb"
 
 pid = spawn("ruby " + file + argString)
