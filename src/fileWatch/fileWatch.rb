9309377ecb88f9c5732ae66e14ce03b6ef8eee06
suhussai <suhussai@ualberta.ca>
2016-02-15 23:27:19 -0700

    add tracking functionality for alter and destroy watch. No timing feature...

--- src/fileWatch/fileWatch.rb
+++ src/fileWatch/fileWatch.rb
@@ -1,12 +1,40 @@
+require 'rb-inotify'
 module FileWatch
 
-    def FileWatchCreation(duration, files)
+    def FileWatchCreation(duration, files, &action)
+        # create_thread = Thread.new do
+        # end
+        # create_thread.priority = -2;
     end
 
-    def FileWatchAlter(duration, files)
+    def FileWatchAlter(duration, files, &action)
+        watcher = INotify::Notifier.new
+        case files
+        when Array
+            files.each {
+                |file| watcher.watch(file, :modify, &action)
+            }
+        when String
+            watcher.watch(files, :modify, &action)
+        end
+        alter_thread = Thread.new do
+            watcher.run
+        end
     end
 
-    def FileWatchDestroy(duration, files)
+    def FileWatchDestroy(duration, files, &action)
+        watcher = INotify::Notifier.new
+        case files
+        when Array
+            files.each {
+                |file| watcher.watch(file, :delete, &action)
+            }
+        when String
+            watcher.watch(files, :delete, &action)
+        end
+        destory_thread = Thread.new do
+            watcher.run
+        end
     end
 
 end
